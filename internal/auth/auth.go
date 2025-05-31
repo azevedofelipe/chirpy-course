0b5999d49499804166e4a4b4c9a47f1e7eba449c
azevedofelipe <felipe.azevedo@dna.tec.br>
2025-05-30 23:47:58 -0300

    Create JWT Tokens and Validate token

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,10 @@
 package auth
 
 import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -16,4 +20,49 @@ func HashPassword(password string) (string, error) {
 
 func CheckPasswordHash(hash, password string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+
+	claims := &jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
+		Subject:   userID.String(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signed, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+
+	return signed, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	type customClaims struct {
+		jwt.RegisteredClaims
+	}
+
+	token, err := jwt.ParseWithClaims(tokenString, &customClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	userIDString, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	userID, err := uuid.Parse(userIDString)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return userID, nil
+
 }
--- internal/auth/hash_test.go
+++ internal/auth/hash_test.go
@@ -2,13 +2,36 @@ package auth
 
 import (
 	"testing"
+	"time"
+
+	"github.com/google/uuid"
 )
 
-func FuncTestHashPassword(t *testing.T) {
+func TestHashPassword(t *testing.T) {
 	password := "testing"
 	hashed, err := HashPassword(password)
 	err = CheckPasswordHash(hashed, "testing")
 	if err != nil {
 		t.Errorf("Password does not match hash: %n", err)
+	}
+}
+
+func TestJWT(t *testing.T) {
+	userID := uuid.New()
+	tokenSecret := "potato"
+	expires := 10 * time.Minute
+
+	tokenString, err := MakeJWT(userID, tokenSecret, expires)
+	if err != nil {
+		t.Errorf("Erro ao criar JWT: %v", err)
+	}
+
+	validatedID, err := ValidateJWT(tokenString, tokenSecret)
+	if err != nil {
+		t.Errorf("Erro ao validar JWT: %v", err)
+	}
+
+	if validatedID != userID {
+		t.Errorf("IDs não coincidem: esperado %v, obtido %v", userID, validatedID)
 	}
 }
